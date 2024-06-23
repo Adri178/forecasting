e32f45a67bda9ca0aa726bde8c3a9670e5b4e693
Berat Sebelah Code <113747080+Adri178@users.noreply.github.com>
2024-06-24 02:48:09 +0700

    Update prediksi_co2.py

--- prediksi_co2.py
+++ prediksi_co2.py
@@ -9,7 +9,7 @@ df = pd.read_excel("CO2 dataset.xlsx")
 df['Year'] = pd.to_datetime(df['Year'], format='%Y')
 df.set_index(['Year'], inplace=True)
 
-st.title('Forecasting CO2')
+st.title('Data Mining - Forecasting CO2')
 year = st.slider("Tentukan Tahun",1,30,step = 1)
 
 pred = model.forecast(year)
